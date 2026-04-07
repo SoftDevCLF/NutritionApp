efa3620c5cde2a21b2226abd968f29b0e9ce2d38
SoftDevCLF <cintya.lara.f@gmail.com>
2026-04-06 19:55:45 -0600

    Add New components to front end

--- Frontend/app/page.js
+++ Frontend/app/page.js
@@ -3,6 +3,9 @@ import VisualizationCards from "./components/VisualizationCards";
 import Filters from "./components/Filters";
 import Pagination from "./components/Pagination";
 import Footer from "./components/Footer";
+import SecurityCompliance from "./components/SecurityCompliance";
+import OAuth2FA from "./components/OAuth2FA";
+import CloudCleanup from "./components/CloudCleanup";
 
 export default function Home() {
   return (
@@ -15,6 +18,9 @@ export default function Home() {
           </h2>
           <VisualizationCards />
           <Filters />
+          <SecurityCompliance />
+          <OAuth2FA />
+          <CloudCleanup />
           <h2 className="text-2xl font-semibold mt-5 mb-4">Pagination</h2>
           <Pagination />
         </main>
